a40c632fa8d9052e568278fb2527b6438ff38400
Muhammad Zeshan Tufail <zraja5226@gmail.com>
2022-03-23 16:46:41 +0500

    initial commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>Our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Exercitationem dolorem enim sapiente. Voluptates illo dolorum enim,
+            perferendis numquam ipsam tempore fugiat error consequuntur
+            consectetur magni quo aliquid porro necessitatibus autem. Hic nihil
+            laboriosam inventore aliquam natus distinctio qui est facere quia.
+            Commodi vel ratione officia. Incidunt illum perferendis laudantium
+            praesentium natus provident consequuntur ullam est tenetur?
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+export default AboutPage;
